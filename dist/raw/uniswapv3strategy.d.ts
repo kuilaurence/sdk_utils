a4982a83ce15dbc0160a9624768436bc86baad1d
kuilaurence <kuilaurence@163.com>
2021-05-26 16:02:36 +0800

    1.0.81

--- dist/raw/uniswapv3strategy.d.ts
+++ dist/raw/uniswapv3strategy.d.ts
@@ -48,11 +48,21 @@ export declare const uniswapv3strategy: ({
         type: string;
     }[];
     name: string;
-    outputs: {
+    outputs: ({
         internalType: string;
         name: string;
         type: string;
-    }[];
+        components?: undefined;
+    } | {
+        components: {
+            internalType: string;
+            name: string;
+            type: string;
+        }[];
+        internalType: string;
+        name: string;
+        type: string;
+    })[];
     stateMutability: string;
     type: string;
     anonymous?: undefined;
