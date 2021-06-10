e1b023b50785406270deb84dcea6b078bbf71894
kuilaurence <kuilaurence@163.com>
2021-06-10 18:53:41 +0800

    fix

--- src/index.ts
+++ src/index.ts
@@ -103,12 +103,13 @@ export async function workers() {
  * @param ratio 价格
  * @returns 
  */
-async function getTick(token0_address: string, token1_address: string, ratio: number) {
+async function getTick(token0_address: string, token1_address: string, price: number) {
   if (Number(token0_address) > Number(token1_address)) {
     let temp = token0_address;
     token0_address = token1_address;
     token1_address = temp;
   }
+  let ratio = 1 / price * 1e12;
   let val0 = Math.log2(ratio);
   let val1 = Math.log2(1.0001);
   let ans = Math.floor(val0 / val1);
@@ -236,8 +237,8 @@ export async function withdraw(token_address: string, amount: string, callback: 
  */
 export async function invest(token0_address: string, token1_address: string, fee: string, amount0: string, amount1: string, leftPrice: string, rightPrice: string, callback: (code: number, hash: string) => void) {
   let v3strategyContract = new web3.eth.Contract(UNISWAPV3STRATEGY, ContractAddress[userInfo.chainID].v3strategy);
-  let tickLower = await getTick(token0_address, token1_address, +leftPrice);
-  let tickUpper = await getTick(token0_address, token1_address, +rightPrice);
+  let tickLower = await getTick(token0_address, token1_address, +rightPrice);//左右
+  let tickUpper = await getTick(token0_address, token1_address, +leftPrice);//交换
   let bigAmount0 = convertNormalToBigNumber(amount0, await getDecimal(token0_address));
   let bigAmount1 = convertNormalToBigNumber(amount1, await getDecimal(token1_address));
   executeContract(v3strategyContract, "invest", 0, [
